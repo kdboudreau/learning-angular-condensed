2aa4b2ebe6066f542d80a313aef9da29f5447dab
Kathryn Boudreau <kdboudreau1@gmail.com>
2020-12-03 16:08:17 -0400

    second class assignment adding cart component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { UserComponent } from './user.component';
 import { FormsModule } from '@angular/forms';
 import { DashboardComponent } from './dashboard.component';
 import { UserDetailComponent } from './user-detail/user-detail.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
     DashboardComponent,
-    UserDetailComponent
+    UserDetailComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
